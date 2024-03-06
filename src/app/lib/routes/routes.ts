5553d8c9a646222d70e15a6b7af48cf3f40ad092
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2024-03-06 16:08:03 +0100

    update API url

--- src/app/lib/routes/routes.ts
+++ src/app/lib/routes/routes.ts
@@ -2,7 +2,7 @@
 export const ROUTE_BASE = "https://moon.warn.group";
 
 // API
-export const ROUTE_API_BASE = "http://127.0.0.1:8000"
+export const ROUTE_API_BASE = "https://moon-api.warn.group"
 
 // Public
 export const ROUTE_ROOT = "/";
