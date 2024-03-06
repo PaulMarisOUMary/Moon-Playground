d9621afd3bc35aaaae511a7e563b2dc09842d6d3
Paul MarisOUMary <paul.marisoumary@gmail.com>
2024-03-06 01:57:13 +0100

    add Request and Response interfaces

---
+++ src/app/lib/models/execute.response.ts
@@ -0,0 +1,7 @@
+export declare interface IExecuteResponse {
+    session_code: string,
+    status: "completed" | "waiting" | "error",
+    prompt: string | null,
+    output: string,
+    errors: string | null,
+}
---
+++ src/app/lib/models/executeinput.request.ts
@@ -0,0 +1,4 @@
+export declare interface IExecuteInputRequest {
+    session_code: string,
+    input: string,
+}
